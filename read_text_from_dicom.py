ba56dd0d6b1678836709d6642d3f2541fc8d5d18
shwetakadupccm <shwetakadu.prashanticancercare@gmail.com>
2021-09-14 11:45:25 +0530

    slice location extracted

--- read_text_from_dicom.py
+++ read_text_from_dicom.py
@@ -1,3 +1,4 @@
+import re
 import pydicom
 import os
 import pandas as pd
@@ -21,12 +22,12 @@ import pandas as pd
 # patient_name, patient_age, file_uid, institute_name, file_description = get_dicom_file_data(file)
 
 
-# varsha_chitre_folder = 'D:/Shweta/radiology_dicom/dicom_image_folder/43_18_Varsha_Chitre/1.3.12.2.1107.5.12.7.3430.30000018011607462201500000117'
-# file_name1 = '1.3.12.2.1107.5.12.7.3430.30000018011607462201500000009.dic'
-# file_path = os.path.join(varsha_chitre_folder, file_name1)
-#
-# file_info = pydicom.read_file(file_path)
-# file_info.dir('pat')
+varsha_chitre_folder = 'D:/Shweta/radiology_dicom/dicom_image_folder/43_18_Varsha_Chitre/1.3.12.2.1107.5.12.7.3430.30000018011607462201500000117'
+file_name1 = '1.3.12.2.1107.5.12.7.3430.30000018011607462201500000009.dic'
+file_path = os.path.join(varsha_chitre_folder, file_name1)
+
+file_info = pydicom.read_file(file_path)
+file_info.dir('pat')
 
 # code_id = file_info[0x0054, 0x0220][0]
 # code_value = code_id.values
@@ -48,75 +49,52 @@ def get_code_type_from_meaning(code_meaning):
     else:
         return None
 
-def get_dicom_file_data(dicom_folder_path):
+def get_image_type(series_description):
+    if 'Segmentation' in series_description:
+        return 'tomo'
+    elif 'Diagnosis' in series_description:
+        return 'mammogram'
+    else:
+        return None
+
+def get_dicom_file_data(dicom_folder_path, path_to_replace = 'D:\\Shweta\\radiology_dicom\\dicom_image_folder\\'):
     dicom_file_data = []
     for root, dirs, files in os.walk(dicom_folder_path):
         for file in files:
             if file.endswith('.dic'):
                 file_path = os.path.join(root, file)
+                print(file_path)
                 dir_path = os.path.dirname(file_path)
+                # print(dir_path)
+                cleaned_dir_path = dir_path.replace(path_to_replace, '')
                 file_txt = pydicom.read_file(file_path)
                 patient_name = file_txt.PatientName
                 patient_age = file_txt.PatientAge
                 file_uid = file_txt.SOPInstanceUID
                 img_laterality = file_txt.ImageLaterality
                 img_laterality_cleaned = clean_img_laterality(img_laterality)
-                slice_lst = file_txt.dir('slice')
-                if 'SliceLocation' in slice_lst:
-                    slice_loc = file_txt.SliceLocation
-                else:
-                    slice_loc = None
                 sr_des = file_txt.SeriesDescription
                 file_description = file_txt.StudyDescription
                 code_id = file_txt[0x0054, 0x0220][0]
                 code_meaning = code_id['CodeMeaning'].value
                 code_type = get_code_type_from_meaning(code_meaning)
-                output_lst = [file, dir_path, patient_name, patient_age, file_uid, img_laterality_cleaned, slice_loc,
-                                  code_meaning, code_type, sr_des, file_description]
+                image_type = get_image_type(sr_des)
+                slice_lst = file_txt.dir('slice')
+                if 'SliceLocation' in slice_lst:
+                    slice_loc = file_txt.SliceLocation
+                    new_file_name = img_laterality_cleaned + '_' + code_type + '_' + image_type + '_' + str(slice_loc)
+                else:
+                    slice_loc = None
+                    new_file_name = img_laterality_cleaned + '_' + code_type + '_' + image_type
+                output_lst = [file, cleaned_dir_path, patient_name, patient_age, file_uid, img_laterality_cleaned, slice_loc,
+                                  code_meaning, code_type, sr_des, file_description, image_type, new_file_name]
                 dicom_file_data.append(output_lst)
     colnames = ['file_name', 'file_path', 'patient_name', 'patient_age', 'file_uid', 'image_laterality',
-                'slice_location', 'code_meaning', 'code_type', 'series_description', 'file_description']
+                'slice_location', 'code_meaning', 'code_type', 'series_description', 'file_description', 'image_type',
+                'new_file_name']
     output_df = pd.DataFrame(dicom_file_data, columns=colnames)
     return output_df
 
 output_df = get_dicom_file_data('D:\\Shweta\\radiology_dicom\\dicom_image_folder')
-output_df.to_excel('D:\\Shweta\\radiology_dicom\\dicom_image_folder\\output_df\\2021_08_30_dicom_file_data_sk.xlsx', index=False)
+output_df.to_excel('D:\\Shweta\\radiology_dicom\\dicom_image_folder\\output_df\\2021_08_31_dicom_file_data_with_new_names_sk.xlsx', index=False)
 
-# def get_dicom_file_data(dicom_folder_path):
-#     file_names = []
-#     dir_paths = []
-#     patient_names = []
-#     patient_ages = []
-#     uids = []
-#     series_description = []
-#     series_number = []
-#     file_description_lst = []
-#     for root, dirs, files in os.walk(dicom_folder_path):
-#         for file in files:
-#             if file.endswith('.dic'):
-#                 file_names.append(file)
-#                 file_path = os.path.join(root, file)
-#                 dir_path = os.path.dirname(file_path)
-#                 dir_paths.append(dir_path)
-#                 file_txt = pydicom.read_file(file_path)
-#                 patient_name = file_txt.PatientName
-#                 patient_names.append(patient_name)
-#                 patient_age = file_txt.PatientAge
-#                 patient_ages.append(patient_age)
-#                 file_uid = file_txt.SOPInstanceUID
-#                 uids.append(file_uid)
-#                 sr_des = file_txt.SeriesDescription
-#                 series_description.append(sr_des)
-#                 sr_num = file_txt.SeriesNumber
-#                 series_number.append(sr_num)
-#                 file_description = file_txt.StudyDescription
-#                 file_description_lst.append(file_description)
-#     output_df = pd.DataFrame(file_names, columns=['file_name'])
-#     output_df['file_path'] = dir_paths
-#     output_df['patient_name'] = patient_names
-#     output_df['patient_age'] = patient_ages
-#     output_df['file_uid'] = uids
-#     output_df['series_description'] = series_description
-#     output_df['series_number'] = series_number
-#     output_df['file_description'] = file_description_lst
-#     return output_df
